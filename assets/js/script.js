262d43de8deb538e79ee5dc4564026ec8d27e71b
JlSanchez05 <jlsanchez2405@gmail.com>
2022-06-23 16:07:25 -0400

    empty

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,7 @@ let createTaskHandler = function(){
     listItemEl.className = 'task-item'
     listItemEl.textContent = 'This is a new task.'
     tasksToDoEl.appendChild(listItemEl)
-
 } 
-
 buttonEl.addEventListener('click',createTaskHandler)
 
 

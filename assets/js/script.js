c95006bb989f1405892609c9b385ca6f5f99d157
JlSanchez05 <jlsanchez2405@gmail.com>
2022-06-23 18:35:07 -0400

    refactored the code and made it more efficneint

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,27 +10,26 @@ Then using appendChild to put the new item into the ui
 
 */
 
-let createTaskHandler = function(event){
+let taskFormHandler = function(event){
     event.preventDefault()
-
+// this give us the value of the text withing that element
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // we can see the value being printed with consoleLog
     console.dir(taskNameInput)
     console.log(taskTypeInput)
-
-    //create lst item
-    let listItemEl = document.createElement('li')
-    listItemEl.className = 'task-item'
     
-    //create div to hold task info and add to list item
-    let taskInfoEl = document.createElement('div')
-    // give it a class name
-    taskInfoEl.className = 'task-info'
-    // add html content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    listItemEl.appendChild(taskInfoEl)
-    //add entire list item to list
-    tasksToDoEl.appendChild(listItemEl)
+    //package up data as an object
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    } 
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj)
+
+   
 /*
     let listItemEl = document.createElement('li')
     listItemEl.className = 'task-item'
@@ -39,7 +38,31 @@ let createTaskHandler = function(event){
     */
  
 } 
-formEl.addEventListener('submit',createTaskHandler)
+
+let createTaskEl = function(taskDataObj){
+  
+ //create list item
+    let listItemEl = document.createElement('li')
+    listItemEl.className = 'task-item'
+    
+    //create div to hold task info and add to list item
+    let taskInfoEl = document.createElement('div')
+    // give it a class name
+    taskInfoEl.className = 'task-info'
+    // add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl)
+    //add entire list item to list
+    tasksToDoEl.appendChild(listItemEl)
+
+
+
+
+}
+
+
+
+formEl.addEventListener('submit',taskFormHandler)
 
 
 

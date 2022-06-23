0e3364a217009b9e4a69993d6c21aedf1cdbf06b
JlSanchez05 <jlsanchez2405@gmail.com>
2022-06-23 18:23:35 -0400

    added more code to the function to make it more dyanmic and make it display the task name and type

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,31 @@ Then using appendChild to put the new item into the ui
 
 let createTaskHandler = function(event){
     event.preventDefault()
+
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.dir(taskNameInput)
+    console.log(taskTypeInput)
+
+    //create lst item
     let listItemEl = document.createElement('li')
     listItemEl.className = 'task-item'
-    listItemEl.textContent = 'This is a new task.'
+    
+    //create div to hold task info and add to list item
+    let taskInfoEl = document.createElement('div')
+    // give it a class name
+    taskInfoEl.className = 'task-info'
+    // add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl)
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl)
+/*
+    let listItemEl = document.createElement('li')
+    listItemEl.className = 'task-item'
+    listItemEl.textContent = taskNameInput
+    tasksToDoEl.appendChild(listItemEl)
+    */
  
 } 
 formEl.addEventListener('submit',createTaskHandler)
